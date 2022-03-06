a7f1ba792c77aeb2e869a9bb49d1aae51f46aa17
Conor Kennedy <conor@conorkennedy.com>
2022-03-06 17:57:27 -0500

    tsx no fun

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,43 @@
 import React from 'react';
-import './App.css';
+import './styles/App.css';
 import YugiohCard from './components/YugiohCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <YugiohCard name='Blue Eyes Ultimate Dragon' img='https://ms.yugipedia.com//8/87/BlueEyesUltimateDragon-LDS2-EN-UR-1E.png' atk='4500' def='3800'/>
-      </header>
-    </div>
-  );
+
+class App extends React.Component<any, any>{
+  constructor(props: any){
+    super(props)
+    this.state={
+      cards: [],
+      DataisLoaded: false
+    };
+  }
+
+  componentDidMount() {
+    fetch(
+      "https://db.ygoprodeck.com/api/v7/cardinfo.php")
+      .then((res) => res.json())
+      .then((json) =>{
+        this.setState({
+          cards: json,
+          DataIsLoaded: true
+        });
+      })
+  }
+
+  render() {
+    const {DataisLoaded, cards} = this.state;
+    return(
+      <div className='container'>
+        <main className='main'>
+          <div className="cardLayout">
+              <YugiohCard name='Blue Eyes Ultimate Dragon' img='https://ms.yugipedia.com//8/87/BlueEyesUltimateDragon-LDS2-EN-UR-1E.png' atk='4500' def='3800'/>
+              <YugiohCard name='Blue Eyes Ultimate Dragon' img='https://ms.yugipedia.com//8/87/BlueEyesUltimateDragon-LDS2-EN-UR-1E.png' atk='4500' def='3800'/>
+              <YugiohCard name='Blue Eyes Ultimate Dragon' img='https://ms.yugipedia.com//8/87/BlueEyesUltimateDragon-LDS2-EN-UR-1E.png' atk='4500' def='3800'/>
+          </div>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/YugiohCard.tsx
+++ src/components/YugiohCard.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import styles from "./YugiohCard.module.css";
+
 interface card{
     name: string,
     img: string,
@@ -8,15 +10,22 @@ interface card{
 }
 const YugiohCard = (props: card) =>{
     return(
-        <React.Fragment>
-            <h2>{props.name}</h2>
-            <img
-                alt={props.name}
-                src={props.img}
-            />
-            <p>Atk: {props.atk}</p>
-            <p>Def: {props.def}</p>
-        </React.Fragment>
+        <a className={styles.cardLink}>
+            <div className={styles.container}>
+                <h2 className={styles.cardHeader}>{props.name}</h2>
+                <div className={styles.cardImageWrapper}>
+                    <img
+                        className={styles.cardImage}
+                        alt={props.name}
+                        src={props.img}
+                        width={360}
+                        height={450}
+                    />
+                </div>
+                <p>Atk: {props.atk}</p>
+                <p>Def: {props.def}</p>
+            </div>
+        </a>
     )
 }
 
